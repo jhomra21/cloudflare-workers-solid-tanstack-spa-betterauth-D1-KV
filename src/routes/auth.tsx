ea2c39ff25719d3353e0d9a7520157440c7be350
jhomra21 <jhonra@icloud.com>
2025-09-08 12:44:48 -0500

    fix: login button using wrong icon and not our icon component

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -14,6 +14,7 @@ import {
 } from '~/lib/auth-actions';
 import type { User, Session } from 'better-auth';
 import { LoginMethodButton } from '~/components/auth/LoginMethodButton';
+import { Icon } from '~/components/ui/icon';
 
 type SessionQueryResult = {
     user: User,
@@ -210,14 +211,7 @@ function AuthPage() {
                 class="mt-4"
                 loading={loadingAction() === 'google'}
                 disabled={loadingAction() !== null}
-                icon={
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="mr-2 h-4 w-4">
-                    <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
-                    <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
-                    <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.223,0-9.641-3.219-11.303-7.583l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
-                    <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571l6.19,5.238C42.022,36.407,44,30.638,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
-                  </svg>
-                }
+                icon={<Icon name="google" />}
                 onClick={() => {
                   setLoadingAction('google');
                   googleSignInMutation.mutate(undefined, {
